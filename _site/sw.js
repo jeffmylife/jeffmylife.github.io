70e690f1d30f0b2597c3b07455ed1467c5955e8d
Jeffrey Lemoine <jlemoine@plexium.com>
2020-08-24 10:03:34 -0700

    twitter feed

--- _site/sw.js
+++ _site/sw.js
@@ -1,10 +1,10 @@
-var CACHE_NAME = "pixyll2-20200824073325";
+var CACHE_NAME = "pixyll2-20200824100321";
 
 self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/pixyll.css?202008240733",
+        "/css/pixyll.css?202008241003",
         "/"
       ]);
     })
