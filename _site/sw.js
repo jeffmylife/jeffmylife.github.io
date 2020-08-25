b059d080d977b7d44d5da9db304708f001851411
Jeffrey Lemoine <jlemoine@plexium.com>
2020-08-25 08:35:24 -0700

    fix mobile

--- _site/sw.js
+++ _site/sw.js
@@ -1,10 +1,10 @@
-var CACHE_NAME = "pixyll2-20200825081427";
+var CACHE_NAME = "pixyll2-20200825082718";
 
 self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/pixyll.css?202008250814",
+        "/css/pixyll.css?202008250827",
         "/"
       ]);
     })
